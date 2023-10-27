a3584bdd67be3a68e576231c3349012ff51867c7
Matthew Weiss <62402516+thalictroides@users.noreply.github.com>
2023-10-27 13:50:08 -0700

    Update multx.sh

--- multx.sh
+++ multx.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Set SLURM options here
-#SBATCH --job-name=processing_job
+#SBATCH --job-name=fastqmultx
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=20:00:00
+#SBATCH --output=fastqmultx_error_%j.log     # Standard output and error log
 
 fastq-multx/fastq-multx -B sep_barcodes.txt -m 0 \
     I1.fastq.gz \
