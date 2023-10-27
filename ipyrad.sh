670a81abe95d9d0d8e1658452beda796e19d7094
Matthew Weiss <62402516+thalictroides@users.noreply.github.com>
2023-10-27 13:48:44 -0700

    Update ipyrad.sh

--- ipyrad.sh
+++ ipyrad.sh
@@ -5,9 +5,7 @@
 #SBATCH --time 4-0:00:00
 #SBATCH --mem-per-cpu 6gb
 #SBATCH --job-name ipyrad_mive
-#SBATCH --output=parallel_%j.log     # Standard output and error log
-#SBATCH --mail-type=BEGIN,END,FAIL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=mw955@nau.edu     # Where to send mail
+#SBATCH --output=ipyrad_error_%j.log     # Standard output and error log
 
 module load mambaforge/22.9.0
 mamba activate iprenv
